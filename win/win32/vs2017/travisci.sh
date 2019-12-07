ee647c00ca8d25eb62159e46a0e7a10c9af85133
nhmall <nhmall@nethack.org>
2019-12-06 20:58:12 -0500

    add steps to obtain Lua and de-gzip it and untar it

--- win/win32/vs2017/travisci.sh
+++ win/win32/vs2017/travisci.sh
@@ -24,9 +24,14 @@ export LIB=/c/Program\ Files\ \(x86\)/Microsoft\ Visual\ Studio/$VSVER/$TOOLSVER
 export LIB=/c/Program\ Files\ \(x86\)/Microsoft\ Visual\ Studio/$VSVER/$TOOLSVER/VC/Tools/MSVC/$MSVER/lib/x86:$LIB
 export LIB=/c/Program\ Files\ \(x86\)/Windows\ Kits/10/lib/$WKITVER/ucrt/x86:$LIB
 export LIB=/c/Program\ Files\ \(x86\)/Windows\ Kits/10/lib/$WKITVER/um/x86:$LIB
-git clone --depth 1 https://github.com/wmcbrine/PDCurses.git ../pdcurses
+mkdir -p lib
+git clone --depth 1 https://github.com/wmcbrine/PDCurses.git lib/pdcurses
+cd lib
+curl -R -O http://www.lua.org/ftp/lua-5.3.5.tar.gz
+tar zxf lua-5.3.5.tar.gz
+cd ../
 export ADD_CURSES=Y
-export PDCURSES_TOP=../../pdcurses
+export PDCURSES_TOP=../lib/pdcurses
 export
 cd src
 cp ../sys/winnt/Makefile.msc ./Makefile
