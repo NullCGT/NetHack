aac90672063aba4958b1668e66e7ef1a58c6c60c
nhmall <nhmall@nethack.org>
2019-12-06 21:19:13 -0500

    windows script update for travis

--- win/win32/vs2017/travisci.sh
+++ win/win32/vs2017/travisci.sh
@@ -28,7 +28,6 @@ mkdir -p lib
 cd lib
 git clone --depth 1 https://github.com/wmcbrine/PDCurses.git pdcurses
 git clone --depth 1 https://github.com/universal-ctags/ctags.git ctags
-cd lib
 curl -R -O http://www.lua.org/ftp/lua-5.3.5.tar.gz
 tar zxf lua-5.3.5.tar.gz
 cd ctags
